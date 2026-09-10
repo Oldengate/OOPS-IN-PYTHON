2610714729da1a7df45ea0658f6a711bce6e3ced
Prithvi Singh <prithvisingh77772209@gmail.com>
2026-09-10 23:00:09 +0530

    Polymorphism by Dukctyping

---
+++ ducktyping.py
@@ -0,0 +1,25 @@
+#"If it looks like a duck, swims like a duck, and quacks like a duck, then it's probably a duck."->polymorphism
+class Duck:  
+    def quack(self):
+        print("Duck: Quack!")
+
+class Person:
+    def quack(self):
+        print("Person: I can quack too!")
+
+
+def make_it_quack(thing):
+    thing.quack()
+
+
+duck = Duck()
+person = Person()
+
+make_it_quack(duck)
+make_it_quack(person)
+
+# Duck: Quack!
+# Person: I can quack too!
+
+
+# ** Process exited - Return Code: 0 **
